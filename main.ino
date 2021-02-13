bca103ad12821b3ffc316a552b53c4e0e3ade787
Juan Blanc <juanblanc14@gmail.com>
2021-02-13 17:07:38 -0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,68 @@
+/*****************************/
+/*                           */
+/* Developed by Juan Blanc   */
+/*                           */
+/*        Jan 2021           */
+/*****************************/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+#include <LiquidCrystal_I2C.h>
+#define direccionLCD 0x27
+LiquidCrystal_I2C lcd(direccionLCD,20,4);
+void setup() {
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+ /* Se inicializa el LCD */
+  lcd.init();
+    lcd.backlight();
+     
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("SSID", "passwd");
+  
+  timeClient.begin();
+
+  lcd.setCursor(0,0);
+    lcd.print(F("Connectig to Wifi  "));
+  
+  while (WiFi.status() != WL_CONNECTED) { delay(500);  lcd.print("."); } /* This loop will print a line each time than is trying to connect to the AP */
+
+  lcd.clear();
+    lcd.setCursor(0,0);
+      lcd.print(" Time in the world!");
+}
+
+void loop() {
+  
+//IST  
+timeClient.update();
+timeClient.setTimeOffset(19800); //GMT -5:30 : The math is -5 * 60 * 60
+lcd.setCursor(0,1);
+lcd.print(timeClient.getFormattedTime());
+lcd.setCursor(9,1);
+lcd.print("India") ;
+
+//EST
+timeClient.update();
+timeClient.setTimeOffset(-18000); //GMT -5 : The math is -5 * 60 * 60
+lcd.setCursor(0,2);
+lcd.print(timeClient.getFormattedTime());
+lcd.setCursor(9,2);
+lcd.print("Eastern") ;
+
+//ARS 
+timeClient.update();
+timeClient.setTimeOffset(-10800);
+lcd.setCursor(0,3);
+lcd.print(timeClient.getFormattedTime());
+lcd.setCursor(9,3);
+lcd.print("Argentina") ; 
+  delay(1000);
+}
